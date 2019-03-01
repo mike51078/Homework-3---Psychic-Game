09220b2308c38904c6ca158763b1183a4705a0ab
elcid00 <elcid00@aol.com>
2019-02-28 19:18:13 -0500

    so very close  7pm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,22 @@
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var answer = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(answer);
 var userGuess;
 var wins = 0;
 var losses = 0;
 var guessesLeft = 11;
 var guessesSoFar = [];
+
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesLeft-text");
 var guessesSoFarText = document.getElementById("guessesSoFar-text");
 
+alert("Do you want to play a game?  Press a letter key to try and guess which letter the computer has chosen.  Can you guess the letter in less than 10 tries?")
 
 document.onkeyup = function (event)  {
-    var userGuess = event.key;
+         userGuess = event.key;
         guessesSoFar.push(event.key);
         guessesSoFarText.textContent = "Your guesses so far: " + guessesSoFar;
 
